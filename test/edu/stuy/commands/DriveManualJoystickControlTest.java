1a737ac2170c59130b05a90ccf72fef5aec8a38f
Blake Elias <blakeelias@gmail.com>
2012-01-26 13:10:21 -0500

    Drive the robot for 5 seconds, check that both wheels have moved.

--- test/edu/stuy/commands/DriveManualJoystickControlTest.java
+++ test/edu/stuy/commands/DriveManualJoystickControlTest.java
@@ -10,7 +10,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import edu.stuy.InitTests;
+
+import edu.stuy.*;
+import edu.wpi.first.wpilibj.*;
+import crio.hardware.*;
 
 /**
  *
@@ -24,11 +27,21 @@ public class DriveManualJoystickControlTest {
     @BeforeClass
     public static void setUpClass() throws Exception {
         InitTests.setUpTests();
+        JoeBot j = new JoeBot();
+        j.robotInit();
+
+        // "Drive" the joysticks
+        Joystick.setStickAxis(RobotMap.LEFT_JOYSTICK_PORT, 1, -1);
+        Joystick.setStickAxis(RobotMap.RIGHT_JOYSTICK_PORT, 1, -1);
     }
 
     @AfterClass
     public static void tearDownClass() throws Exception {
         InitTests.tearDownTests();
+
+        // "un-drive" the joysticks
+        Joystick.setStickAxis(RobotMap.LEFT_JOYSTICK_PORT, 1, 0);
+        Joystick.setStickAxis(RobotMap.RIGHT_JOYSTICK_PORT, 1, 0);
     }
     
     @Before
@@ -47,6 +60,16 @@ public class DriveManualJoystickControlTest {
     @Test
     public void testDriveManualJoystickControl() {
         DriveManualJoystickControl cmd = new DriveManualJoystickControl();
-        assertFalse(cmd.isFinished());
+        cmd.initialize();
+        double startTime = Timer.getFPGATimestamp();
+        while (Timer.getFPGATimestamp() - startTime < 5) {
+            cmd.execute();
+        }
+        cmd.end();
+
+        // TODO: get encoder readings from drivetrain object
+        System.out.println(CRIO.client.getdata()[1] + " " + CRIO.client.getdata()[2]);
+        assertTrue(CRIO.client.getdata()[1] > 0);
+        assertTrue(CRIO.client.getdata()[2] > 0);
     }
 }
