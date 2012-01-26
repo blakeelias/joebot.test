bc18512587776cbf5a59f500290dc58b3009d302
Blake Elias <blakeelias@gmail.com>
2012-01-26 13:51:29 -0500

    Automatically start VIRSYS when running tests.  Assumes VIRSYS directory is on the same level as joebot.test

--- test/edu/stuy/InitTests.java
+++ test/edu/stuy/InitTests.java
@@ -2,9 +2,17 @@ package edu.stuy;
 
 import edu.stuy.Devmode;
 import crio.hardware.CRIO;
+import java.io.*;
 
 public class InitTests {
-    public static void setUpTests() {
+    public static void main(String[] args) throws IOException {
+        setUpTests();
+    }
+
+    public static void setUpTests() throws IOException {
+        File f = new File("../VIRSYS/");
+        System.out.println(f.getAbsolutePath());
+        Runtime.getRuntime().exec("./virsys", null, f);
         CRIO.init();
         Devmode.DEV_MODE = true;
     }
